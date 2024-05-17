17e69b2d61e9a87e39275f722cf06c869cb668a9
dev_tahir <kalaiwalatahir@gmail.com>
2024-05-17 22:09:49 +0530

    Worked on header

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -9,13 +9,12 @@ export default function Header() {
         { name: "About me", link: "/" },
         { name: "skills", link: "/" },
         { name: "Projects", link: "/" },
-        { name: "", link: "/" },
+        { name: "Contact Me", link: "/" },
     ];
     const [optionalLink, setoptionalLink] = useState(false);
 
     const openMenu = ()=>{
         optionalLink? document.body.classList.remove('overflow-hidden') : document.body.classList.add('overflow-hidden')
-        
     }
 
     return (
@@ -28,7 +27,7 @@ export default function Header() {
                     </Link>
 
                     {/* Menu Items */}
-                    <ul className={`flex flex-col lg:flex-row fixed lg:relative top-0 left-0 lg:top-auto lg:left-auto bg-[#1b1b1b] lg:space-x-4 ml-auto w-80 z-50 h-screen lg:h-auto lg:w-auto p-8 lg:p-0 shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] lg:shadow-none rounded-xl transition-all duration-300 ease-in-out ${optionalLink ? "left-0 opacity-100" : "-left-full opacity-0"}`}>
+                    <ul className={`flex flex-col lg:flex-row fixed lg:relative top-0 left-0 lg:top-auto lg:left-auto bg-[#1b1b1b] lg:space-x-4 ml-auto w-80 z-50 h-screen lg:h-auto lg:w-auto p-8 lg:p-0 shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] lg:shadow-none rounded-xl transition-all duration-300 ease-in-out ${optionalLink ? "left-0 opacity-100" : "-left-full lg:left-auto opacity-0 lg:opacity-100"}`}>
                         {
                             navLinks.map((Links) => (
                                 <li key={Links.name}>
@@ -39,7 +38,7 @@ export default function Header() {
                     </ul>
 
                     {/* CTA Buttons */}
-                    <div className={optionalLink ? "md:flex space-x-4" : "lg:hidden"} onClick={()=>{setoptionalLink(!optionalLink); openMenu()}}>
+                    <div className={optionalLink ? "flex lg:hidden space-x-4" : "lg:hidden"} onClick={()=>{setoptionalLink(!optionalLink); openMenu()}}>
                         <button className="bg-[#242424] text-white p-3 rounded-full cursor-pointer transition-all duration-300 ease-in-out">
                             {optionalLink ? (
                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
--- components/Hero/index.jsx
+++ components/Hero/index.jsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function heroBanner() {
   return (
     <section className="w-full mt-6">
-      <div className="grid lg:grid-cols-2 gap-6">
+      <div className="grid lg:grid-cols-2 gap-3 sm:gap-5 lg:gap-6">
         <div className="shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] bg-[#1b1b1b] rounded-xl py-4"></div>
-        <div className="text-center shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] bg-[#1b1b1b] rounded-xl py-32">
+        <div className="text-center shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] bg-[#1b1b1b] rounded-xl py-24 lg:py-32">
           <span className="uppercase bg-[#111] text-[#ffca9a] py-1 px-3 rounded-md inline-block">AVAILABLE FOR WORK</span>
           <h1 className="text-6xl font-medium text-white mt-3">Web designer</h1>
           <p className="text-xl text-white opacity-40 max-w-[444px] mx-auto mt-5 mb-7">A passionate SaaS product designer based in the vibrant city of New York.</p>
