9d4017061a43f2208ecc6362f5060a84cb522b90
dev_tahir <kalaiwalatahir@gmail.com>
2024-09-07 00:00:27 +0530

    Upadte ui and add tabs in work history section

---
+++ components/WorkHistory/Tabs/index.jsx
@@ -0,0 +1,38 @@
+import { useState } from 'react';
+
+const Tabs = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
+  const tabs = [
+    { label: 'Experiences', content: 'This is the content of Tab 1' },
+    { label: 'Fun Facts', content: 'This is the content of Tab 2' },
+  ];
+
+  return (
+    <div className="w-full mx-auto">
+      <div className="flex bg-[#161616] rounded-sm py-1 w-fit">
+        {tabs.map((tab, index) => (
+          <button
+            key={index}
+            className={`text-base leading-normal py-1 px-3 font-medium rounded-sm ${activeTab === index ? 'text-black bg-white' : 'text-white'}`}
+            onClick={() => setActiveTab(index)}
+          >
+            {tab.label}
+          </button>
+        ))}
+      </div>
+      <div className="relative 2xl:mt-14 xl:mt-10 lg:mt-8 mt-5">
+        {tabs.map((tab, index) => (
+          <div
+            key={index}
+            className={`transition-opacity duration-500 ease-in-out absolute inset-0 ${activeTab === index ? 'opacity-100' : 'opacity-0'}`}
+          >
+            <p className='text-base text-white'>{tab.content}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Tabs;
--- components/WorkHistory/WorkHistory.jsx
+++ components/WorkHistory/WorkHistory.jsx
@@ -2,6 +2,7 @@
 
 import Image from "next/image";
 import Headings from "../Headings";
+import Tabs from "./Tabs";
 
 
 const WorkHistory = () => {
@@ -13,27 +14,7 @@ const WorkHistory = () => {
             </div>
             <div className="grid md:grid-cols-2 gap-6">
                 <div className="bg-[#1b1b1b] rounded-2xl p-10 shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)]">
-                    <h1 className="text-6xl">Tabs</h1>
-                    <div>
-                        <span className="text-[#ffca9a] text-sm block max-w-fit font-semibold px-3 py-1 rounded-md bg-[#111] shadow-[inset_0_1px_rgba(255,255,255,0.06)]">Work History</span>
-                        <div className="grid grid-cols-3 gap-6">
-                            <div className="rounded-2xl p-6 shadow-[inset_0_1px_rgba(255,255,255,.06)] bg-[#1b1b1b] grid place-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full" viewBox="0 0 48 48" width="96px" height="96px"><path fill="#E65100" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"/><path fill="#FF6D00" d="M24 8L24 39.9 35.2 36.7 37.7 8z"/><path fill="#FFF" d="M24,25v-4h8.6l-0.7,11.5L24,35.1v-4.2l4.1-1.4l0.3-4.5H24z M32.9,17l0.3-4H24v4H32.9z"/><path fill="#EEE" d="M24,30.9v4.2l-7.9-2.6L15.7,27h4l0.2,2.5L24,30.9z M19.1,17H24v-4h-9.1l0.7,12H24v-4h-4.6L19.1,17z"/></svg>
-                            </div>
-                            <div className="rounded-2xl p-6 shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] bg-[#1b1b1b] grid place-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full" viewBox="0 0 48 48" width="96px" height="96px"><path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"/><path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"/><path fill="#FFF" d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"/><path fill="#EEE" d="M24,13v4h-8.9l-0.3-4H24z M19.4,21l0.2,4H24v-4H19.4z M19.8,27h-4l0.3,5.5l7.9,2.6v-4.2l-4.1-1.4L19.8,27z"/></svg>
-                            </div>
-                            <div className="rounded-2xl p-6 shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] bg-[#1b1b1b] grid place-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full" viewBox="0 0 48 48" width="96px" height="96px"><path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"/><path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"/><path fill="#FFF" d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"/><path fill="#EEE" d="M24,13v4h-8.9l-0.3-4H24z M19.4,21l0.2,4H24v-4H19.4z M19.8,27h-4l0.3,5.5l7.9,2.6v-4.2l-4.1-1.4L19.8,27z"/></svg>
-                            </div>
-                            <div className="rounded-2xl p-6 shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] bg-[#1b1b1b] grid place-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full" viewBox="0 0 48 48" width="96px" height="96px"><path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"/><path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"/><path fill="#FFF" d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"/><path fill="#EEE" d="M24,13v4h-8.9l-0.3-4H24z M19.4,21l0.2,4H24v-4H19.4z M19.8,27h-4l0.3,5.5l7.9,2.6v-4.2l-4.1-1.4L19.8,27z"/></svg>
-                            </div>
-                            <div className="rounded-2xl p-6 shadow-[inset_0_1px_0_1px_rgba(255,255,255,0.06)] bg-[#1b1b1b] grid place-items-center">
-                                <svg xmlns="http://www.w3.org/2000/svg" className="h-full w-full" viewBox="0 0 48 48" width="96px" height="96px"><path fill="#0277BD" d="M41,5H7l3,34l14,4l14-4L41,5L41,5z"/><path fill="#039BE5" d="M24 8L24 39.9 35.2 36.7 37.7 8z"/><path fill="#FFF" d="M33.1 13L24 13 24 17 28.9 17 28.6 21 24 21 24 25 28.4 25 28.1 29.5 24 30.9 24 35.1 31.9 32.5 32.6 21 32.6 21z"/><path fill="#EEE" d="M24,13v4h-8.9l-0.3-4H24z M19.4,21l0.2,4H24v-4H19.4z M19.8,27h-4l0.3,5.5l7.9,2.6v-4.2l-4.1-1.4L19.8,27z"/></svg>
-                            </div>
-                        </div>
-                    </div>
+                <Tabs />
                 </div>
                 <div className="rounded-3xl overflow-hidden">
                     <Image src="/history-work.jpg" alt="History Work" width={800} height={600} className="max-w-full h-auto" />
